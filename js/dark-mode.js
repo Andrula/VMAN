c6c70c056dcdf8f79e3fbe40fe5a5662b069ce96
Andreas Rasmussen <xandreas@live.dk>
2023-02-07 12:09:56 +0100

    Fixed a few things

--- js/dark-mode.js
+++ js/dark-mode.js
@@ -1,1 +1,14 @@
+function myAlert(){
+    alert('You have activated dark mode for virtual manager!');
+    var head = document.getElementsByTagName('HEAD')[0];
+    var link = document.createElement('link');
+    link.rel = "stylesheet";
+    link.type = "text/css";
+    link.href = "/css/light-mode.css";
+    head.appendChild(link);
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('darkMode').addEventListener('click', myAlert);
+});
 
